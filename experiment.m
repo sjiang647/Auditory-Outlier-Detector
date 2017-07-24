3c95b2eb5b03e022420176e99a141bea5ded60c1
Andy Peng <andypg20@gmail.com>
2017-07-24 10:30:50 -0700

    Initial experiment file

---
+++ experiment.m
@@ -0,0 +1,26 @@
+%% Experiment 5: auditory outlier detection
+
+clear all;
+close all;
+clc;
+
+%% General Setups (vars, tone ramp, screen, etc.)
+
+%% Input subject name & save
+
+%% Tuning sound (Convert Hz to MIDI semitones)
+
+%% Task instructions 
+
+%% Counterbalancing 
+
+%% Actual experiment 
+
+%% Asking whether high or low
+
+%% Saving response
+
+%% Repeat #6-#8 nIter times 
+
+%% Save result
+

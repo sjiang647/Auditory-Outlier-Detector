d7de49e57d40a05a5dfef4f6982f65b5ee588851
Francis <Francis@calvisitor-10-105-131-114.calvisitor.1918.berkeley.edu>
2017-07-24 11:53:47 -0700

    center

--- experiment.m
+++ experiment.m
@@ -9,14 +9,11 @@ clc;
 % [window, rect] = Screen('OpenWindow', 0);
 % Screen('BlendFunction', window, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 % HideCursor();
-% window_w = rect(3);
-% window_h = rect(4); 
-% 
-% x1 = window_w/2;
-% y1 = window_h/2;
-
 rng('shuffle');
 
+% windowX = rect(3);
+% windowY = rect(4); 
+% center = [windowX/2, windowY/2];
 numTrial = 10;
 outlierRange = [6, 8, 10, 12];
 outlierPos = 1;
@@ -42,7 +39,7 @@ subjectData = {};
 % end
 
 %% Tuning sound (Convert Hz to MIDI semitones)
-midiTones = zeros(1,128);
+
 for midiVal = 1:128
     toneFrequency = 440*2^((midiVal - 69)/12);
     midiTones(midiVal) = sin(2*pi* toneFrequency * toneLength(midiVal));
@@ -50,8 +47,10 @@ end
 c;
 
 %% Task instructions 
+
 %     Screen('DrawText', window, 'You will listen to 7 audio tones. 1 tone is an outlier. If the outlier is a higher tone, press the ?H? key. If the outlier is a lower tone, press the ?L? key.', x1, y1-25);
-%     Screen('Flip',window);  
+%     Screen('Flip',window); 
+
 %% Counterbalancing 
 
 %% Actual experiment 
